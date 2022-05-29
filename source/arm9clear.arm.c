137ecd82f1ade2e6b834b7fc4bc22ad7bae3c796
RocketRobz <bobesh8@gmail.com>
2022-05-29 01:26:18 -0600

    Clear sound capture registers to fix Memory Pit-related RAM issues

--- source/arm9clear.arm.c
+++ source/arm9clear.arm.c
@@ -4,6 +4,7 @@
 #include <nds/ndstypes.h>
 #include <nds/dma.h>
 #include <nds/system.h>
+#include <nds/ipc.h>
 #include <nds/interrupts.h>
 #include <nds/timers.h>
 
@@ -33,6 +34,11 @@ ARM_CODE void __attribute__ ((long_call)) __attribute__((naked)) __attribute__((
 		TIMER_DATA(i) = 0;
 		for(reg=0; reg<0x1c; reg+=4)*((vu32*)(0x04004104 + ((i*0x1c)+reg))) = 0;//Reset NDMA.
 	}
+
+	// Clear out FIFO
+	REG_IPC_SYNC = 0;
+	REG_IPC_FIFO_CR = IPC_FIFO_ENABLE | IPC_FIFO_SEND_CLEAR;
+	REG_IPC_FIFO_CR = 0;
 
 	VRAM_CR = (VRAM_CR & 0xffff0000) | 0x00008080 ;
 	
